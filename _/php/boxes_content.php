e6c90699865e1a0e467d76bfaf1ca245d076ee74
Abraham Taylor <abe.970@gmail.com>
2015-02-22 20:14:03 -0800

    changed some css and look of page

--- _/php/boxes_content.php
+++ _/php/boxes_content.php
@@ -1,41 +1,41 @@
 <div class="clearfix visible-xs visible-lg"></div>
-  <div class="row">
-      <div class="col-lg-3 col-xs-6">
-        <div class="thumbnail">
-              <div class="caption">
-                  <h4>Members</h4>
-                  <p>Information on membership, current members</p>
-                  <p><a href="members.php">Go to member page</a></p>
-              </div>
-          </div>
-      </div>
-      <div class="col-lg-3 col-xs-6">
-           <div class="thumbnail">
-              <div class="caption">
-                  <h4>About us</h4>
-                  <p>Information about MIPOPS</p>
-                  <p><a href="about.php">Go to about us page</a></p>
+<div class="row" id="boxes_content">
+    <div class="col-lg-3 col-xs-6">
+      <div class="thumbnail">
+            <div class="caption">
+                <h4>Members</h4>
+                <p>Information on membership, current members</p>
+                <p><a href="members.php">Go to member page</a></p>
             </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-xs-6">
+         <div class="thumbnail">
+            <div class="caption">
+                <h4>About us</h4>
+                <p>Information about MIPOPS</p>
+                <p><a href="about.php">Go to about us page</a></p>
           </div>
-      </div>
-      <div class="clearfix visible-md visible-xs"></div>
-      <div class="col-lg-3 col-xs-6">
-        <div class="thumbnail">
-              <div class="caption">
-                  <h4>Contact</h4>
-                  <p>Contact MIPOPS</p>
-                  <p><a href="contact.php"> Go to contact us page</a></p>
-              </div>
-          </div>
-      </div>
-      <div class="col-lg-3 col-xs-6">
-        <div class="thumbnail">
-              <div class="caption">
-              <h4>Resources</h4>
-              <p>Check our our resources page</p>
-              <p><a href="resources.php"> Go to resources page</a></p>
+        </div>
+    </div>
+    <div class="clearfix visible-md visible-xs"></div>
+    <div class="col-lg-3 col-xs-6">
+      <div class="thumbnail">
+            <div class="caption">
+                <h4>Contact</h4>
+                <p>Contact MIPOPS</p>
+                <p><a href="contact.php"> Go to contact us page</a></p>
             </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-xs-6">
+      <div class="thumbnail">
+            <div class="caption">
+            <h4>Resources</h4>
+            <p>Check our our resources page</p>
+            <p><a href="resources.php"> Go to resources page</a></p>
           </div>
-       </div>
-  </div><!-- end nested row 3a -->
+        </div>
+     </div>
+</div><!-- end nested row 3a -->
 
--- _/php/members_content.php
+++ _/php/members_content.php
@@ -1,20 +1,23 @@
-<div class="row">
-  <div class="col-xs-6 col-md-3">
+<div class="row" id="members_content">
+  <div class="col-xs-12 col-md-4">
+    <p>Members to check out</p>
+  </div>
+  <div class="col-xs-6 col-md-2">
     <a href="#" class="thumbnail">
       <img src="img/SirMixAlot.jpg" alt="member 1">
     </a>
   </div>
-  <div class="col-xs-6 col-md-3">
+  <div class="col-xs-6 col-md-2">
     <a href="#" class="thumbnail">
       <img src="img/VideoStill.jpg" alt="member 2">
     </a>
   </div>
-  <div class="col-xs-6 col-md-3">
+  <div class="col-xs-6 col-md-2">
     <a href="#" class="thumbnail">
       <img src="img/AMansJob.jpg" alt="member 3">
     </a>
   </div>
-  <div class="col-xs-6 col-md-3">
+  <div class="col-xs-6 col-md-2">
     <a href="#" class="thumbnail">
       <img src="img/DomeInterior.jpg" alt="member 4">
     </a>
