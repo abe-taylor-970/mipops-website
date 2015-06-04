689edadfb0679b8da34bd940ae7b6b2ec3b980db
Abraham Taylor <abe.970@gmail.com>
2015-06-04 11:31:36 -0700

    Added terms.php and privacy.php and changed footer to link to them

--- _/php/footer.php
+++ _/php/footer.php
@@ -1,4 +1,4 @@
 <div class="spacing"></div>
 <footer class="row">
-    <p>© 2015 MIPoPS. All Rights Reserved.</p>
+    <p>© 2015 MIPoPS. All Rights Reserved. || <a href="terms.php">Terms and Conditions</a> || <a href="privacy.php">Privacy Policy</a></p>
 </footer>
---
+++ privacy.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>MIPOPS -- Construction</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "_/php/bootstrap.php"; ?>
+  </head>
+  <body>
+		<section class="container">
+      <?php include "_/php/header.php"; ?>
+      <h3>We do not currently store cookies or any other personal information from our website visitors.</h3>
+      <?php include "_/php/footer.php"; ?>
+    </section><!-- container -->
+    <?php include "_/php/javascript.php"; ?>
+  </body>
+</html>
