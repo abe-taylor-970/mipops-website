9111a687b352fa4ae0b167287df98af6c1c5281e
Abraham Taylor <abe.970@gmail.com>
2015-09-10 20:06:23 -0700

    updated visit page

--- visit.php
+++ visit.php
@@ -15,22 +15,28 @@
     </div><!-- full-width-->
     <div class="spacing"></div>
     <section class="container border" id="visit">
-      <h1>What to Expect on Your First Visit to MIPOPS</h1>
+      <h1>What to expect when you visit us</h1>
 
       <div class="row type2">
+        <p>MIPoPS is a membership-based non-profit, and use of the MIPoPS migration lab requires a service agreement.  To become a MIPoPS member, please contact us.  Reviewing and answering the questions in Planning Your Project will help you define your needs.</p>
+        <br />
+        <p>Following is a guide to using the MIPoPS migration lab:</p>
+        <br />
         <ul>
-          <li>MIPoPS is located on Floor 3 of City Hall, 600 Fourth Avenue, Seattle.  Entrances are on Fourth and Fifth Avenues and on Cherry.  Public transportation is the best option as parking in the vicinity is expensive.</li>
-          <li>Check in with the reception desk for the Office of the City Clerk and we will escort you to the MIPOPS space.</li>  
-          <li><b>Bring a Hard drive formatted for your office environment - windows or mac.</b></li> 
+          <li>Workstation space is limited: contact MIPoPS at least two days in advance to schedule an appointment.</li>
+          <li>MIPoPS is located on Floor 3 of City Hall, 600 Fourth Avenue, Seattle, 98014. Entrances are on Fourth and Fifth Avenues and on Cherry Street. Public transportation is the best option as parking in the vicinity is expensive, though there are several parking garages nearby.</li>
+          <li>Check in with the reception desk for the Office of the City Clerk and we will escort you to the MIPoPS space.</li>
+          <li><b>Bring a portable hard drive formatted for your office environment - windows or mac.</b></li>
           <li>Videos are digitized in <i>real time</i>; you might not want to bring more than you have time for.</li>
+          <li>Be sure to label your cartridge cases and hard drive with your institution’s name.</li>
           <li>Three primary steps included: <ol><li>digitizing video tape</li><li>transcoding resulting .mov file to file format(s) of your choice</li><li>copying files onto your media.</li></ol></li>
-          <li>There is no storage space for either analog objects or digital files. Be prepared to take your items with you at the end of the day.</li>
+          <li>MIPoPS does not provide storage space for either analog objects or digital files. Be prepared to take your items with you at the end of the day.</li>
           <li>Do as much appraisal as possible in advance.</li>
           <li>If possible, tell us in advance which formats you'll be bringing in. We can digitize VHS, U-Matic, Betcam SP, Digital Betacam, Hi 8, Mini DV and DV Cam</li>
-          <li>If labels are not adhered well, please remove them. Have info about what's on the label written somewhere else, if you think you'll need it.</li>
-          <li>Refer to the document: "Preparing Video for Capture"</li> 
-          <li>Let us know what file formats you would like your video transferred to.  The scripts for digitizing into specific formats are written in ffmpeg; if you need formats other than mpeg4 and ffv1 please let us know ahead of time. Please email or call and we can discuss this to you.</li>
-          <li>Expect to work one-on-one with someone on your first visit; it won't be long before you'll be on your own, but even then there will be someone there to help if you need it</li>
+          <li>If labels are not adhered well to your cassettes, please remove them. Have info about what's on the label written somewhere else, if you think you'll need it.</li>
+          <li>Refer to the document: "Preparing Video for Capture"</li>
+          <li>Let us know what file formats you would like your video transferred to. If you need formats other than mpeg4 and ffv1 please let us know ahead of time, as we will need to modify the ffmpeg digital migration script. Please email or call and we can discuss your needs.</li>
+          <li>Expect to work one-on-one with someone on your first visit; it won't be long before you'll be on your own, but even then there will be someone there to help if you need it.</li>
         </ul>
       </div><!-- type 2 row -->
     </section><!-- container -->
