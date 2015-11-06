82e50994f5e237d267ad2cf281a5607d2c147641
Abraham Taylor <abe.970@gmail.com>
2015-11-05 20:18:50 -0800

    Updated carousel

--- _/php/top_carousel.php
+++ _/php/top_carousel.php
@@ -9,17 +9,15 @@
 		<li data-target="#top_carousel" data-slide-to="5"></li>
 		<li data-target="#top_carousel" data-slide-to="6"></li>
 		<li data-target="#top_carousel" data-slide-to="7"></li>
-		<li data-target="#top_carousel" data-slide-to="8"></li>
 	</ol><!-- carousel-indicators -->
 
 	<div class="carousel-inner" role="listbox">
-		<div class="active item"><a href="img/vhs.jpg"><img src="img/vhs_cropped.jpg" alt="vhs" class="img-responsive"></a></div>
-		<div class="item"><a href="img/female_construction_rotated.jpg"><img src="img/female_construction_cropped.jpg" alt="female construction" class="img-responsive"></a></div>
+		<div class="active item"><a href="img/magnetic_media_focussed.jpg"><img src="img/magnetic_media_focussed_cropped.jpg" alt="vhs" class="img-responsive"></a></div>
+		<div class="item"><a href="img/Libby_with_screen.jpg"><img src="img/Libby_with_screen_cropped.jpg" alt="Libby looking at video analytics" class="img-responsive"></a></div>
 		<div class="item"><a href="img/hands_paper_device_rotated.jpg"><img src="img/hands_paper_device_cropped.jpg" alt="hands paper device" class="img-responsive"></a></div>
 		<div class="item"><a href="img/pink_hair_rotated.jpg"><img src="img/pink_hair_cropped.jpg" alt="pink_hair" class="img-responsive"></a></div>
-    <div class="item"><a href="img/devices_and_cables_rotated.jpg"><img src="img/devices_and_cables_cropped.jpg" alt="devices and cables" class="img-responsive"></a></div>
-    <div class="item"><a href="img/people4_rotated.jpg"><img src="img/people4_cropped.jpg" alt="people and devices interacting" class="img-responsive"></a></div>
-    <div class="item"><a href="img/rack_rotated.jpg"><img src="img/rack_cropped.jpg" alt="rack" class="img-responsive"></a></div>
+		<div class="item"><a href="img/image_from_video.jpg"><img src="img/image_from_video_cropped.jpg" alt="Video image" class="img-responsive"></a></div>
+    <div class="item"><a href="img/inserting_into_device.jpg"><img src="img/inserting_into_device_cropped.jpg" alt="Insert into Device" class="img-responsive"></a></div>
     <div class="item"><a href="img/people5.jpg"><img src="img/people5_cropped.jpg" alt="person smiling through rack" class="img-responsive"></a></div>
     <div class="item"><a href="img/woman_with_screens_and_tower.jpg"><img src="img/woman_with_screens_and_tower_scaled.jpg" alt="workstation" class="img-responsive"></a></div>
 	</div><!-- carousel-inner -->
