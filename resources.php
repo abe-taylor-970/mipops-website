9233f5fc1024209c0574350bb57b10d4bc0cce89
Abraham Taylor <abe.970@gmail.com>
2015-12-10 15:24:34 -0800

    slightly changed darkness of links. Added underlines to resources.

--- resources.php
+++ resources.php
@@ -24,75 +24,75 @@
       <ul>
         <li><h3>Digitizing video for long-term preservation: An RFP guide and template</h3>
           <p><i>This document, created by the NYU Video at Risk project, is intended to take an institution step-by-step through the process of drafting a Request for Proposals (RFP) for the transfer of analog video formats to digital carriers for preservation. Whether or not you send a collection to a vendor, this document provides a good roadmap for the entire process.</i></p>
-          <p><a target=frame2 href="dox/VARRFP.pdf">Digitizing Video</a>
+          <p><u><a target=frame2 href="dox/VARRFP.pdf">Digitizing Video</a></u>
         </li>
         <li><h3>Videotape preservation fact sheets</h3>
           <p><i>Compiled by the Association of Moving Image Archivists, this is a good primer on videotape, however it is slightly out of date in its presentation of information regarding reformatting for preservation and tape conversion.</i></p>
-          <p><a target=frame2 href="dox/fact_sheets_0.pdf">Fact Sheets</a></p>
+          <p><u><a target=frame2 href="dox/fact_sheets_0.pdf">Fact Sheets</a></u></p>
         </li>
         <li><h3>Videotape Identification and Assessment Guide</h3>
           <p>Texas Commission on the Arts</p>
           <p><i>An indispensable visual guide to identify videotape formats</i></p>
-          <p><a target=frame2 href="dox/TCoA_guide.pdf">TCoA Guide</a></p>
+          <p><u><a target=frame2 href="dox/TCoA_guide.pdf">TCoA Guide</a></u></p>
           <p>American Archive of Public Broadcasting Listening and Viewing Guide</p>
           <p><i>A helpful guide to efficiently and accurately identifying video content</i></p>
-          <p><a target=frame2 href="dox/ListeningandViewingGuidelines.pdf">Listening and Viewing Guidelines</a></p>
+          <p><u><a target=frame2 href="dox/ListeningandViewingGuidelines.pdf">Listening and Viewing Guidelines</a></u></p>
         </li>
         <li><h3>Guidelines for the creation of digital collections</h3>
           <p>Digitization Best Practices for Moving Images
           <br>CARLI [Consortium of Academic Research Libraries in Illinois] Digital Collections Users' Group
           <br>Standards Subcommittee</p>
           <p><i>This is a very well-written, easy to understand explanation of the digitization process</i></p>
-          <p><a target=frame2 href="dox/guidelines_for_video.pdf">Guidelines for Video</a></p>
+          <p><u><a target=frame2 href="dox/guidelines_for_video.pdf">Guidelines for Video</a></u></p>
         </li>
         <li><h3>Federal agencies digitization guidelines initiative [FADGI]</h3>
           <p><i>Very technical information about metadata and digitization standards. The Audio Visual working group is still drafting recommendations.</i></p>
-          <p><a target=frame2 href="http://www.digitizationguidelines.gov/">http://www.digitizationguidelines.gov/</a></p>
+          <p><u><a target=frame2 href="http://www.digitizationguidelines.gov/">http://www.digitizationguidelines.gov/</a></u></p>
         </li>
         <li><h3>Library of congress sustainability of digital formats, planning for library of congress collections</h3>
           <p><i>Technical information about digital content formats compiled by the Library of Congress</i></p>
-          <p><a target=frame2 href="http://www.digitalpreservation.gov/formats/intro/intro.shtml">http://www.digitalpreservation.gov/formats/intro/intro.shtml</a></p>
+          <p><u><a target=frame2 href="http://www.digitalpreservation.gov/formats/intro/intro.shtml">http://www.digitalpreservation.gov/formats/intro/intro.shtml</a></u></p>
         </li>
         <li><h3>Video at risk: Strategies for preserving commercial video collections in libraries</h3>
           <p><i>Guidelines, written as part of the Video at Risk project, that seek to clarify exemptions for copying audiovisual works under Section 108(c) of the United States Copyright Act and thereby enhance the ability of librarians to preserve their video collections.</i></p>
-          <p><a target=frame2 href="dox/VideoAtRisk_SECTION108_Guidelines_2013.pdf">Video at risk guidelines</a></p>
+          <p><u><a target=frame2 href="dox/VideoAtRisk_SECTION108_Guidelines_2013.pdf">Video at risk guidelines</a></u></p>
         </li>
         <li><h3>Bay Area Video Coalition</h3>
           <p><i>The Bay Area Video Coalition (BAVC) is one of the nation's longest-standing non-profit video and audio preservation organizations. BAVC was and remains a leader in the field, developing the highest quality preservation standards and practices and serving cultural organizations nationally.</i></p>
-          <p><a target=frame2 href="http://www.bavc.org/preservation">http://www.bavc.org/preservation</a></p>
+          <p><u><a target=frame2 href="http://www.bavc.org/preservation">http://www.bavc.org/preservation</a></u></p>
         </li>
         <li><h3>VideoPreservation Website</h3>
-          <p><i>The VideoPreservation Website was conceived and constructed by conservators Timothy Vitale and Paul Messier. The project was made possible through a grant by the <a target=frame2 href="http://www.ncptt.nps.gov">National Center for Preservation Technology and Training</a> and received additional support from the <a target=frame2 href="http://www.bavc.org">Bay Area Video Coalition</a>. The authors wish to acknowledge all of the contributors to the site and especially recognize Walter Henry, creator of <a target=frame2 href="http://cool.conservation-us.org">Conservation OnLine</a>, and Stanford University for hosting this project.</i></p>
-          <p><a target=frame2 href="http://videopreservation.conservation-us.org/index.html">http://videopreservation.conservation-us.org/index.html</a></p>
+          <p><i>The VideoPreservation Website was conceived and constructed by conservators Timothy Vitale and Paul Messier. The project was made possible through a grant by the <u><a target=frame2 href="http://www.ncptt.nps.gov">National Center for Preservation Technology and Training</a></u> and received additional support from the <u><a target=frame2 href="http://www.bavc.org">Bay Area Video Coalition</a></u>. The authors wish to acknowledge all of the contributors to the site and especially recognize Walter Henry, creator of <u><a target=frame2 href="http://cool.conservation-us.org">Conservation OnLine</a></u>, and Stanford University for hosting this project.</i></p>
+          <p><u><a target=frame2 href="http://videopreservation.conservation-us.org/index.html">http://videopreservation.conservation-us.org/index.html</a></u></p>
           <p><i>The VideoPreservation Library page has some really interesting links and is worth taking a look:</i></p>
-          <p><a target=frame2 href="http://videopreservation.conservation-us.org/library/index.html">http://videopreservation.conservation-us.org/library/index.html</a></p>
+          <p><u><a target=frame2 href="http://videopreservation.conservation-us.org/library/index.html">http://videopreservation.conservation-us.org/library/index.html</a></u></p>
           <p><i>The Video Guide, a contemporary book about videotape published in 1981, is especially interesting for understanding the history of videotape and how it was used:</i></p>
-          <p><a target=frame2 href="http://videopreservation.conservation-us.org/vid_guide/front/front.html">http://videopreservation.conservation-us.org/vid_guide/front/front.html</a></p>
+          <p><u><a target=frame2 href="http://videopreservation.conservation-us.org/vid_guide/front/front.html">http://videopreservation.conservation-us.org/vid_guide/front/front.html</a></u></p>
         </li>
         <li><h3>American archive of public broadcasting cataloging guidelines</h3>
           <p><i>Cataloging Guidelines for creating quality metadata for audio visual materials from 2015.</i></p>
-          <p><a target=frame2 href="dox/CatalogingGuidelines.pdf">Cataloging Guidelines</a></p>
+          <p><u><a target=frame2 href="dox/CatalogingGuidelines.pdf">Cataloging Guidelines</a></u></p>
         </li>
         <li><h3>American archive of public broadcasting listening and viewing guidelines</h3>
           <p><i>Guidelines to be used for both listening to sound assets and viewing moving image assets. While there are many similarities in cataloging moving image and sound assets, there are some fundamental differences in the type of information each type of asset conveys.</i></p>
-          <p><a target=frame2 href="dox/ListeningandViewingGuidelines.pdf">Listening and Viewing Guidelines</a></p>
+          <p><u><a target=frame2 href="dox/ListeningandViewingGuidelines.pdf">Listening and Viewing Guidelines</a></u></p>
         </li>
         <li><h3>"Good Enough" Digital Preservation Solutions for Under-Resourced Cultural Heritage Institutions:</h3>
-          <p><a target=frame2 href="dox/FromTheoryToAction_POWRR_WhitePaper.pdf">From Theory to Action White Paper</a></p>
+          <p><u><a target=frame2 href="dox/FromTheoryToAction_POWRR_WhitePaper.pdf">From Theory to Action White Paper</a></u></p>
           <h3>Using the Internet Archive: A guide created by the Digital POWRR Project:</h3>
-          <p><a target=frame2 href="dox/Internet-Archive-Tutorial1.pdf">Internet Archive Tutorial</a></p>
+          <p><u><a target=frame2 href="dox/Internet-Archive-Tutorial1.pdf">Internet Archive Tutorial</a></u></p>
           <h3>About POWRR:</h3>
-          <p><a target=frame2 href="http://digitalpowrr.niu.edu/">http://digitalpowrr.niu.edu/</a></p>
+          <p><u><a target=frame2 href="http://digitalpowrr.niu.edu/">http://digitalpowrr.niu.edu/</a></u></p>
         </li>
         <li><h3>AVCC Cataloging toolkit</h3>
-          <p><a target=frame2 href="http://keepingcollections.org/avcc-cataloging-toolkit/">http://keepingcollections.org/avcc-cataloging-toolkit/</a></p>
+          <p><u><a target=frame2 href="http://keepingcollections.org/avcc-cataloging-toolkit/">http://keepingcollections.org/avcc-cataloging-toolkit/</a></u></p>
         </li>
         <li><h3>A Primer on Codecs for Moving Image and Sound Archives</h3>
-          <p><a target=frame2 href="dox/AVPS_Codec_Primer.pdf">AVPS Codec Primer</a></p>
+          <p><u><a target=frame2 href="dox/AVPS_Codec_Primer.pdf">AVPS Codec Primer</a></u></p>
         </li>
         <li><h3>Guide for archiving digital video.</h3>
           <p><i>This is a good resource for born digital materials.</i></p>
-          <p><a target=frame2 href="http://archiveguide.witness.org/">Archivists' Guide to Archiving Video</a></p>
+          <p><u><a target=frame2 href="http://archiveguide.witness.org/">Archivists' Guide to Archiving Video</a></u></p>
         </li> 
       </ul>
     </div>
