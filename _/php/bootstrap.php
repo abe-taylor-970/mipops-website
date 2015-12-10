28e16abca953c55b0620c6917e7db0394c05644a
Abraham Taylor <abe.970@gmail.com>
2015-12-10 14:33:45 -0800

    bootstrap changed for bw file

--- _/php/bootstrap.php
+++ _/php/bootstrap.php
@@ -1,4 +1,4 @@
 <!-- Bootstrap -->
 <link href="_/css/bootstrap.min.css" rel="stylesheet">
-<link href="_/css/carol_style.css" rel="stylesheet">
+<link href="_/css/bw_style1.css" rel="stylesheet">
 <script src="_/js/respond.min.js"></script>
