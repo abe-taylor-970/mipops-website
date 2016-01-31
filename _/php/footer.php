d2ee3bd88a1afe61a6f19d71f019ab70759cf6cb
Abraham Taylor <abe.970@gmail.com>
2016-01-31 12:58:55 -0800

    changed copyright date to 2016. Added some pics and updated todo

--- _/php/footer.php
+++ _/php/footer.php
@@ -1,4 +1,4 @@
 <div class="spacing"></div>
 <footer class="row type1">
-    <p>&#169; 2015 MIPoPS. All Rights Reserved. || <a href="terms.php">Terms and Conditions</a> || <a href="privacy.php">Privacy Policy</a></p>
+    <p>&#169; 2016 MIPoPS. All Rights Reserved. || <a href="terms.php">Terms and Conditions</a> || <a href="privacy.php">Privacy Policy</a></p>
 </footer>
