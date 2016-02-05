05231259a33f144d910f3dc0c36e00354cb9357d
Abraham Taylor <abe.970@gmail.com>
2016-02-05 13:10:29 -0800

    Fixed about to work on mobile

--- about.php
+++ about.php
@@ -47,10 +47,10 @@
     <div class="row">
       <div class="col-xs-12 border members hidden-small2">
         <h1>Hannah Palin</h1>
+        <img src="img/bios/Hannah.jpg" alt="Hannah Palin" class="right-big img-responsive">
         <a href="http://www.lib.washington.edu/specialcollections" class="left">
           <img src="img/uw_libraries.png" alt="University of Washington Libraries" class="img-responsive"/>
         </a>
-        <img src="img/bios/Hannah.jpg" alt="Hannah Palin" class="right hidden-small3">
         <p>Hannah Palin helped to create the moving image preservation program at the UW Libraries, Special Collections and has been working on moving image projects there for over a decade. In her work as the Moving Image Archives Specialist, she has managed a number of grant projects including the Washington Film Preservation Project, in which Special Collections performed preservation work and conducted workshops for nine regional institutions that did not have the ability to preserve their film collections, including Seattle Municipal Archives, the Museum of History and Industry, the Museum of Flight, the Yakama Nation, and the Burke Museum. She co-authored The Washington State Film Preservation Manual: Low-cost and No-cost Suggestions to Care for your Film with Nicolette Bromberg, Visual Materials Curator for the UW Libraries Special Collections. They also co-authored a recent article in the Association of Moving Image Archivists journal The Moving Image,  "Starting from Nothing, The Art of Creating a Film Archive." She has recently completed work on a major multi-year project funded by the National Endowment for the Humanities to preserve the Mountaineers Film Collection. She taught workshops on moving image preservation for the Society of American Archivists, the University of Oregon, and the University of Washington. She is also one of the founders of Moving Image Preservation of Puget Sound, a collaborative project to assist small, regional heritage institutions preserve their videotape collections. Ms. Palin worked in both film and radio production and, before coming to Special Collections, she used to spend forty hours a week, sitting in the dark, watching home movies in a local film transfer lab.</p>
         <p><a href="mailto:hannah@mipops.org">hannah@mipops.org</a></p>
       </div>
@@ -59,10 +59,10 @@
     <div class="row">
       <div class="col-xs-12 border members hidden-small2">
         <h1>Carol Shenk</h1>
+        <img src="img/bios/carol_cropped.jpg" alt="Carol Shenk" class="right-big img-responsive">
         <a href= "http://www.kingcounty.gov/depts/records-licensing/archives.aspx" class="left">
           <img src="img/king_county.jpg" alt="King County Archives" class="img-responsive small-height"/>
         </a>
-        <img src="img/bios/carol_cropped.jpg" alt="Carol Shenk" class="right hidden-small3">
         <p>Carol Shenk has served as King County Archivist in Seattle, Washington, since 2013. Before joining King County, Carol served as Information Manager for the Seattle City Clerk and Municipal Archives where she managed online databases, led digitization projects, and helped develop the Municipal Archives’ Digital Assets Preservation program.  Prior to that, Carol served as records manager and public disclosure officer for the City of Shoreline; was a team lead in Amazon.com’s catalog group; and served several stints as clerk and then librarian at Municipal Research & Services Center of Washington. Carol earned her Master of Library and Information Science degree from the University of Washington in 1998 and has a BA in Fine Arts from the University of Oregon.  Carol is a member of the Academy of Certified Archivists.</p>
         <p><a href="mailto:carol@mipops.org">carol@mipops.org</a></p>
       </div>
@@ -82,10 +82,10 @@
     <div class="row">
       <div class="col-xs-12 border members hidden-small2">
         <h1>Anna Briggs</h1>
+        <img src="img/bios/anna_briggs_cropped.jpg" alt="Anna Briggs" class="right-big img-responsive">
         <a href= "http://www.univ-tours.fr/" class="left">
           <img src="img/universite_de_tours_cropped.png" alt="Universite De Tours, France" class="img-responsive"/>
         </a>
-        <img src="img/bios/anna_briggs_cropped.jpg" alt="Anna Briggs" class="right hidden-small3">
         <p>Anna Briggs is a moving image archivist specialised in amateur and documentary film curation, film literacy, archival outreach and programming. She is currently a Ph.D. candidate at the Université François-Rabelais de Tours (France): the subject of her research is non-fiction moving images as archival and curatorial objects.  She has recently presented her research at the Library of Congress' Cultural Heritage Archive Symposium and at the 21st Visible Evidence international conference in Delhi.   Anna is also a lecturer, teaching  cataloguing and  documentary film analysis. She is an expert for the Education, Audiovisual and Culture Executive Agency of the European Commission, and is a board member of non-profit organisations Moving Image Preservation of Puget Sound and Xcape, a community recording studio in Saint-Pierre-des-Corps (France).</p>
         <p><a href="mailto:annab@mipops.org">annab@mipops.org</a></p>
       </div>
