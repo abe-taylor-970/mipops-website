23b608d55a9e82b2006f99faabd607a7001da7cf
Abraham Taylor <abe.970@gmail.com>
2016-09-21 09:07:34 -0700

    Added spaces to planning, visit. Adjusted style slightly on picture for those pages.

--- planning.php
+++ planning.php
@@ -22,18 +22,30 @@
       <a href="img/freeway_park.png" class="float-right hidden-small3"><img src="img/freeway_park_cropped_and_scaled.jpg" alt="Freeway Park" class="img-responsive" /></a>
       <ul>
         <li>How many tapes do you have?</li>
+        <br />
         <li>For your first visit, bring <u>two or three tapes</u> so we can assess the condition and perhaps take a look at the content. ALSO, bring a <u>portable hard drive</u> formatted for your office environment - Windows or Mac.</li>
 <!-- [add in calculations for hard drive size] should be added to last line -->
+        <br />
         <li>Do you know the formats of your tapes? See <a href="http://www.mipops.org/dox/TCoA_guide.pdf">Videotape Identification and Assessment Guide</a>.</li>
+        <br />
         <li>We can digitize VHS, U-Matic, Betcam SP, Digital Betacam, 1” open reel, D2, Hi 8, MiniDV and DVCam.</li>
+        <br />
         <li>Do you know what the content is?</li>
+        <br />
         <li>Have you done appraisal? We can appraise in-house, but it’s helpful to do what you can in advance.</li>
+        <br />
         <li>If labels are not adhered well to your cassettes, please remove them, but keep the label or have info about what's on the label written somewhere else, if you think it’s accurate.</li>
+        <br />
         <li>How many videotapes do you want to digitize? Having a catalog entry and an item number is helpful for tracking your video through the digitization process.</li>
+        <br />
         <li>Videos are digitized in real time; you might not want to bring more than you have time for during your allotted time.</li>
+        <br />
         <li>We will discuss the correct preservation and access file formats for your institution. Once the digitization is done you have not only the original videotape but associated digital files.</li>
+        <br />
         <li>Consider how you describe and store the files you receive at your home institutions. We can talk to you about this.</li>
+        <br />
         <li>Which digitized videos do you want to put on the Internet Archive? See <a href="http://www.mipops.org/access.php">Making Your Videos Accessible</a>.</li>
+        <br />
         <li>Are there any copyright restrictions?</li>
 <!-- There should be another list item saying See "file formats" with hyperlink-->
     </ul>
--- visit.php
+++ visit.php
@@ -23,9 +23,13 @@
       <a href="img/woman_adjusting_dial.jpg" class="float-right hidden-small3"><img src="img/woman_adjusting_dial_scaled.jpg" alt="Woman adjusting dial" class="img-responsive" /></a>
       <ul>
         <li>Contact MIPoPS at least a week in advance to schedule an appointment.</li>
+        <br />
         <li>MIPoPS is located on the 3<sup>rd</sup> floor of Seattle City Hall, 600 Fourth Avenue, Seattle, 98104. Entrances are on Fourth and Fifth Avenues, between James and Cherry.</li>
+        <br />
         <li>Check in with the reception desk for the Office of the City Clerk on the 3<sup>rd</sup> Floor and we will escort you to the MIPoPS space.</li>
+        <br />
         <li>As a non-profit, we differ in what we offer our fellow non-profit clients. We are not really a drop-off transfer house. Your organization sends a volunteer or staff person to participate in the process, which can include helping decide which tapes to transfer, a discussion of how to manage the files that are provided and as much hands-on digitizing as that person feels comfortable with. We also ask that at least some digitized files be put up at the<a href="https://archive.org/details/mipops">MIPoPS Internet Archive site</a>. See <a href="http://www.mipops.org/access.php">Making Your Videos Accessible</a>.</li>
+        <br />
         <li>Please see <a href="http://www.mipops.org/members-area/mipops_guest_manual_5-19-2016.pdf">MIPoPS Guest Manual</a> (members only).</li>
       </ul>
     </div><!-- type 2 row -->
