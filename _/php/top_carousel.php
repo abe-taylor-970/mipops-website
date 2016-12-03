8982fbae2886c1be3612b700a078f12e4e40d009
Abraham Taylor <abe.970@gmail.com>
2016-12-03 11:53:45 -0800

    Re-ordered pictures in top_carousel

--- _/php/top_carousel.php
+++ _/php/top_carousel.php
@@ -14,12 +14,12 @@
 	<div class="carousel-inner" role="listbox">
 		<div class="active item"><a href="http://www.mipops.org/img/Libby_with_screen.jpg"><img src="http://www.mipops.org/img/Libby_with_screen_cropped.jpg" alt="Libby looking at video analytics" class="img-responsive carousel-image"></a></div>
 		<div class="item"><a href="http://www.mipops.org/img/hands_paper_device_rotated.jpg"><img src="http://www.mipops.org/img/hands_paper_device_cropped.jpg" alt="hands paper device" class="img-responsive carousel-image"></a></div>
-		<div class="item"><a href="http://www.mipops.org/img/pink_hair_rotated.jpg"><img src="http://www.mipops.org/img/pink_hair_cropped.jpg" alt="pink_hair" class="img-responsive carousel-image"></a></div>
 		<div class="item"><a href="http://www.mipops.org/img/image_from_video.jpg"><img src="http://www.mipops.org/img/image_from_video_cropped.jpg" alt="Video image" class="img-responsive carousel-image"></a></div>
-    <div class="item"><a href="http://www.mipops.org/img/Libby_adjustment.jpg"><img src="http://www.mipops.org/img/Libby_adjustment_cropped.jpg" alt="Libby adjusts a dial" class="img-responsive carousel-image"></a></div>
-    <div class="item"><a href="http://www.mipops.org/img/people5.jpg"><img src="http://www.mipops.org/img/people5_cropped.jpg" alt="Rachel smiling through rack" class="img-responsive carousel-image"></a></div>
-    <div class="item"><a href="http://www.mipops.org/img/inserting_into_device.jpg"><img src="http://www.mipops.org/img/inserting_into_device_cropped.jpg" alt="Insert into Device" class="img-responsive carousel-image"></a></div>
     <div class="item"><a href="http://www.mipops.org/img/woman_with_screens_and_tower.jpg"><img src="http://www.mipops.org/img/woman_with_screens_and_tower_scaled.jpg" alt="workstation" class="img-responsive carousel-image"></a></div>
+    <div class="item"><a href="http://www.mipops.org/img/people5.jpg"><img src="http://www.mipops.org/img/people5_cropped.jpg" alt="Rachel smiling through rack" class="img-responsive carousel-image"></a></div>
+    <div class="item"><a href="http://www.mipops.org/img/Libby_adjustment.jpg"><img src="http://www.mipops.org/img/Libby_adjustment_cropped.jpg" alt="Libby adjusts a dial" class="img-responsive carousel-image"></a></div>
+    <div class="item"><a href="http://www.mipops.org/img/inserting_into_device.jpg"><img src="http://www.mipops.org/img/inserting_into_device_cropped.jpg" alt="Insert into Device" class="img-responsive carousel-image"></a></div>
+		<div class="item"><a href="http://www.mipops.org/img/pink_hair_rotated.jpg"><img src="http://www.mipops.org/img/pink_hair_cropped.jpg" alt="pink_hair" class="img-responsive carousel-image"></a></div>
 	</div><!-- carousel-inner -->
 
 	<a href="#top_carousel" class="left carousel-control" role="button" data-slide="prev">
