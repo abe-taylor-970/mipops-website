43234f6aba2bce5758f326352995df80d916bbc9
Abraham Taylor <abe.970@gmail.com>
2016-12-06 15:24:44 -0800

    Updated link to video on main page

--- _/php/video.php
+++ _/php/video.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="col-lg-11 col-lg-offset-1" id="video"> 
-    <iframe src="https://www.youtube.com/embed/oK7BSi06RUU" id="main_iframe" width="100%" height="405" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/JzVBPmkevMM?list=PLxhePmBncJGwkleukvccLEPbD-b9eC8a0" id="main_iframe" width="100%" height="405" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
   </div>
 </div>
 
