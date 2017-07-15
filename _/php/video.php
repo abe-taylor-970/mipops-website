dd0dc8cdc48f8fc900d02c07d01d83be3e874683
Abraham Taylor <abe.970@gmail.com>
2017-07-14 18:33:15 -0700

    Reverted website back to old video from Rachels blog

--- _/php/video.php
+++ _/php/video.php
@@ -1,9 +1,7 @@
 <div class="row">
   <div class="col-lg-11 col-lg-offset-1" id="video"> 
-    <div class="video-container" id="main-iframe-container">
-      <iframe id="main_iframe" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FMovingImagePreservationofPugetSound%2Fvideos%2F1396107660480958%2F&show_text=0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
-    </div>
-    <p id="text-for-iframe" class="blue-link-inside border-line">Moving History Strikes Back! The next MIPoPS archival screening night at the <a class="normal-size" target="frame2" href="http://www.nwfilmforum.org/">Northwest Film Forum</a> will be Thursday, June 22nd at 7:30PM. Join us at 6:30 for happy hour in the lobby. A big thank you to <a class="normal-size" target="frame2" href="https://www.facebook.com/CloudPerson/">Cloud Person</a> for letting us use their music.</p>
+    <iframe id="main_iframe" src="https://www.youtube.com/embed/JzVBPmkevMM" width="100%" height="86.4%" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
+    <p class="blue-link-inside border-line">Natural Resources Defense Council spot with Carroll O'Connor. See <a class="normal-size" href="http://mipops.tumblr.com/">Rachel's blog</a> for more details.</p>
   </div>
 </div>
 
