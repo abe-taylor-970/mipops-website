a9e5d9d2565181ca35155d5315afa1d523bdacc1
Abraham Taylor <abe.970@gmail.com>
2018-01-25 10:48:45 -0800

    Changed video for next Moving History event. Now, the CSS doesn't need to be changed for these events, just the video.php

--- _/php/video.php
+++ _/php/video.php
@@ -1,9 +1,9 @@
 <div class="row">
   <div class="col-lg-11 col-lg-offset-1" id="video"> 
     <div class="video-container" id="main-iframe-container">
-      <iframe id="main_iframe" src="https://www.youtube.com/embed/JzVBPmkevMM" width="100%" height="86.4%" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
+      <iframe id="main_iframe" src="https://www.youtube.com/embed/HSPh5CeH7hg" width="100%" height="86.4%" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
     </div>
-    <p class="blue-link-inside border-line">Natural Resources Defense Council spot with Carroll O'Connor. See <a class="normal-size" href="http://mipops.tumblr.com/">Rachel's blog</a> for more details.</p>
+    <p class="blue-link-inside border-line">Our next Moving History event is coming up at <a class="normal-size" href="https://nwfilmforum.org/films/moving-history-from-tape-to-terabyte/">Northwest Film Forum</a> on Sunday, February 11th from 5-7PM (Happy Hour at 4PM). Come join us!</p>
   </div>
 </div>
 
--- _/php/video_old.php
+++ _/php/video_old.php
@@ -1,6 +1,8 @@
 <div class="row">
   <div class="col-lg-11 col-lg-offset-1" id="video"> 
-    <iframe id="main_iframe" src="https://www.youtube.com/embed/JzVBPmkevMM" width="100%" height="86.4%" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
+    <div class="video-container" id="main-iframe-container">
+      <iframe id="main_iframe" src="https://www.youtube.com/embed/JzVBPmkevMM" width="100%" height="86.4%" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
+    </div>
     <p class="blue-link-inside border-line">Natural Resources Defense Council spot with Carroll O'Connor. See <a class="normal-size" href="http://mipops.tumblr.com/">Rachel's blog</a> for more details.</p>
   </div>
 </div>
