38e7e985e13527c27b11d8f8bdd59f6abb4899f6
Abraham Taylor <abe.970@gmail.com>
2018-01-25 13:02:23 -0800

    Updated footer with 2018 date

--- _/php/footer.php
+++ _/php/footer.php
@@ -1,4 +1,4 @@
 <div class="spacing"></div>
 <footer class="row type1">
-    <p>&#169; 2016-2017 MIPoPS. All Rights Reserved. || <a href="http://www.mipops.org/terms.php">Terms and Conditions</a> || <a href="http://www.mipops.org/privacy.php">Privacy Policy</a></p>
+    <p>&#169; 2016-2018 MIPoPS. All Rights Reserved. || <a href="http://www.mipops.org/terms.php">Terms and Conditions</a> || <a href="http://www.mipops.org/privacy.php">Privacy Policy</a></p>
 </footer>
