1e8ab57d1f42d9e6c82c75414fc800ce1b9e4321
Abraham Taylor <abe.970@gmail.com>
2018-01-26 12:24:31 -0800

    Updated website to use fifth testimonial.

--- _/php/bootstrap.php
+++ _/php/bootstrap.php
@@ -1,4 +1,5 @@
 <!-- Bootstrap -->
 <link href="_/css/bootstrap.min.css" rel="stylesheet">
 <link href="_/css/bw_style.css" rel="stylesheet">
+<link href="_/css/normalize.css" rel="stylesheet">
 <script src="_/js/respond.min.js"></script>
