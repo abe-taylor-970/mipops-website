3647ec3547148164760ac39e8eadc2626820ec9d
Abraham Taylor <abe.970@gmail.com>
2018-01-30 15:36:17 -0800

    Added new link to video. Updated todo.

--- _/php/video.php
+++ _/php/video.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-lg-11 col-lg-offset-1" id="video"> 
     <div class="video-container" id="main-iframe-container">
-      <iframe id="main_iframe" src="https://www.youtube.com/embed/HSPh5CeH7hg" width="100%" height="86.4%" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
+      <iframe id="main_iframe" src="https://www.youtube.com/embed/JeVFSVL4eKs" width="100%" height="86.4%" scrolling="no" frameborder="0" allowFullScreen="true" webkitallowfullscreen mozallowfullscreen></iframe>
     </div>
     <p class="blue-link-inside border-line">Our next Moving History event is coming up at <a class="normal-size" href="https://nwfilmforum.org/films/moving-history-from-tape-to-terabyte/">Northwest Film Forum</a> on Sunday, February 11th from 5-7PM (Happy Hour at 4PM). Come join us!</p>
   </div>
